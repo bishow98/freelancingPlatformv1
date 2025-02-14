26b6c9d01501f6d1d3aea965e892c6b9d91257e5
Bishow Chauhan <chauhandavid4199@gmail.com>
2025-02-14 23:16:45 +0545

    fetch recommended jobs for freelancers; enhance LatestJobs component with user role check and display logic

--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -1,55 +1,72 @@
-import { useSelector } from "react-redux"
-import LatestJobsCards from "./LatestJobsCards"
-// import { useEffect } from "react";
-// import { JOB_RECOMMENDED_API_END_POINT } from "./utils/constants";
-// import axios from "axios";
-// import { setRecommendedJobs } from "../redux/jobSlice";
-
-
+import { useDispatch, useSelector } from "react-redux";
+import LatestJobsCards from "./LatestJobsCards";
+import { JOB_RECOMMENDED_API_END_POINT } from "./utils/constants";
+import axios from "axios";
+import { useEffect } from "react";
+import { setRecommendedJobs } from "../redux/jobSlice";
 
 const LatestJobs = () => {
+  const { allJobs, recommendedJobs } = useSelector((store) => store.job);
+  const { user } = useSelector((store) => store.auth);
 
-    const {allJobs} = useSelector((store)=>store.job);
-    // const dispatch = useDispatch();
-    // useEffect(()=> {
-    //   const getAllRecommendedJobs = async() =>{
-    //     try {
-    //       const res = await axios.get(`${JOB_RECOMMENDED_API_END_POINT}/recommend-jobs`);
-    //       console.log(res.data);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const getAllRecommendedJobs = async () => {
+      try {
+        const res = await axios.get(
+          `${JOB_RECOMMENDED_API_END_POINT}/recommend-jobs`,
+          { withCredentials: true }
+        );
+        console.log(res.data);
 
-    //       if(res.data.success){
-    //         dispatch(setRecommendedJobs(res.data.alldata))
-    //       }
-          
-    //     } catch (error) {
-    //       console.log(`Error at Latest Jobs with job recommended type: ${error.message}`)
-          
-    //     }
-    //   }
-    //   getAllRecommendedJobs();
-
-    // },[])
+        if (res.data.success) {
+          dispatch(setRecommendedJobs(res.data.alldata));
+        }
+      } catch (error) {
+        console.log(
+          `Error at Latest Jobs with job recommended type: ${error.message}`
+        );
+      }
+    };
+    getAllRecommendedJobs();
+  }, []);
   return (
     <div className="max-w-7xl mx-auto my-20">
-        <div className="flex flex-col items-center ">
-        <h1 className="text-4xl font-bold">Latest <span className="text-[#6A38C2]"> Job Openings</span></h1>
+      <div className="flex flex-col items-center ">
+        <h1 className="text-4xl font-bold">
+          Latest <span className="text-[#6A38C2]"> Job Openings</span>
+        </h1>
+      </div>
+      <div className="grid grid-cols-3 gap-4 mt-5">
+        {allJobs.length <= 0 ? (
+          <span>No Job Available </span>
+        ) : (
+          allJobs
+            ?.slice(0, 6)
+            .map((job) => <LatestJobsCards key={job._id} job={job} />)
+        )}
+      </div>
+
+      {user && user.role === "freelancer" && (
+        <div>
+          <div className="flex flex-col items-center">
+            <h1 className="text-4xl font-bold mt-10">
+              Recommended <span className="text-[#6A38c2]">Jobs</span>
+            </h1>
+          </div>
+          <div className="grid grid-cols-3 gap-4 mt-5">
+            {recommendedJobs?.recommendations?.length <= 0 ? (
+              <span className="text-lg font-bold">No any recommended Jobs</span>
+            ) : (
+              recommendedJobs?.recommendations?.map((job) => (
+                <LatestJobsCards key={job._id} job={job} />
+              ))
+            )}
+          </div>
         </div>
-        <div className="grid grid-cols-3 gap-4 mt-5">
-            {allJobs.length <= 0? <span>No Job Available </span>:allJobs?.slice(0,6).map((job)=><LatestJobsCards key={job._id} job={job}/>)}
-        </div>
-        <div className="flex flex-col items-center">
-        <h1 className="text-4xl font-bold mt-10">Recommended <span className="text-[#6A38c2]">Jobs</span></h1>
-        </div>
-
-        {/* <div className="grid grid-cols-3 gap-4 mt-5">
-          <LatestJobsCards/>
-
-        </div> */}
-
-
-
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default LatestJobs
+export default LatestJobs;
